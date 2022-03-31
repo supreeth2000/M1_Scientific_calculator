46ab5c041e363dd14378220f0c16e1c0c30ac324
Supreeth_GL <102604134+supreeth2000@users.noreply.github.com>
2022-03-31 18:27:10 +0530

    Add files via upload

---
+++ 3_Implementation/test/funtions.h
@@ -0,0 +1,16 @@
+int add(int a,int b);
+float subtract(float a,float b);
+float multiply(float a,float b);
+float divide(float a,float b);
+int fact(int n);
+int npr(int n,int r);
+int ncr(int n,int r);
+ int power(int x,int y);
+ void quadratic_roots(double a_1,double b_1,double c_1);
+ float exponential(float x);
+ float sine_value(float x);
+ float cos_value(float x);
+ float tan_value(float x);
+int determinant();
+float square(float x);
+float cube(float x);
